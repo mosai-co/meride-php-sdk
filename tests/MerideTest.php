c04ee58ad235d86971dc8724aa8bd92ac400aff2
Toppi Giovanni Manuel <giovanni.toppi@mosai.co>
2019-08-30 17:00:05 +0200

    aggiornato test Meride

--- tests/MerideTest.php
+++ tests/MerideTest.php
@@ -15,26 +15,26 @@ final class MerideTest extends TestCase
     public function testGetVideoNotEmpty()
     {
         $api = new Api($this->MERIDE_ACCESS_TOKEN, $this->MERIDE_URL);
-        $this->assertNotEmpty($api->get("video", 883));
+        $this->assertNotEmpty($api->get("video", 919));
     }
 
     public function testGetVideoNotEmptyVersion2()
     {
         $api = new Api($this->MERIDE_ACCESS_TOKEN, $this->MERIDE_URL, $this->MERIDE_VERSION);
-        $this->assertNotEmpty($api->get("video", 883));
+        $this->assertNotEmpty($api->get("video", 919));
     }
 
     public function testGetVideoJsonContentIsValid()
     {
         $api = new Api($this->MERIDE_ACCESS_TOKEN, $this->MERIDE_URL);
-        $videoResponse = $api->get("video", 883);
+        $videoResponse = $api->get("video", 919);
         $this->assertInstanceOf(stdClass::class, $videoResponse->jsonContent);
     }
 
     public function testGetVideoHasContent()
     {
         $api = new Api($this->MERIDE_ACCESS_TOKEN, $this->MERIDE_URL);
-        $videoResponse = $api->get("video", 883);
+        $videoResponse = $api->get("video", 919);
         $this->assertNotEmpty($videoResponse->content);
     }
 }

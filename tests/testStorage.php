6922d656233631627b538200ed5a81318047c47b
Toppi Giovanni Manuel <giovanni.toppi@mosai.co>
2020-09-02 19:55:02 +0200

    [skip ci] modifiche test storage

--- tests/testStorage.php
+++ tests/testStorage.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-//use Meride\Api;
+use Meride\Api;
 use Meride\Storage\Tus\Client as TusClient;
 use Meride\Storage\Tus\Token;
 
@@ -14,7 +14,7 @@ $videoOrigin = __DIR__."/assets/small.mp4";
 
 
 //$merideApi = new Api(MERIDE_ACCESS_TOKEN, MERIDE_URL, MERIDE_VERSION);
-$tokenGenerator = new Token('webink', getenv('MERIDE_AUTH_CODE'), 'http://localhost:3010');
+$tokenGenerator = new Token('webink', getenv('MERIDE_AUTH_CODE'));
 try {
     $token = $tokenGenerator->generate();
 } catch(\Exception $e) {
@@ -22,8 +22,7 @@ try {
 }
 
 $tusClient = new TusClient(
-    $token,
-    "http://localhost:3010"
+    $token
 );
 $tusClient->setProtocol("http");
 $uploadUrl = $tusClient->upload($videoOrigin, 'smaller.mp4');
@@ -31,6 +30,12 @@ if ($uploadUrl !== false) {
     echo "\r\nUploaded ".$uploadUrl;
     $extractedUrl = $tusClient->extractURL($uploadUrl);
     echo "\r\nVideo URL ".$extractedUrl."\r\n\r\n";
+    $merideApi = new Api(MERIDE_ACCESS_TOKEN, MERIDE_URL, MERIDE_VERSION);
+    $videoResponse = $merideApi->create('video', array(
+        'title' => "Test video",
+        'video' => $extractedUrl
+    ));
+    echo var_dump($videoResponse);
 } else {
     echo "Some error occured";
 }
